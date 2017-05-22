7d935d422fe8444756e066dad8a07765a30d83db
Sam Neubardt <samneubardt@gmail.com>
2017-05-22 19:16:13 -0400

    Publish attachments to a pubsub topic
    so they can be retrieved async

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # snacks
-https://snacksnacksnacksnacksnack
+
+## Initial Setup
+1. `cd functions && yarn install`
+
+## GCP Configuration
+
+### Topics
+The following must be created:
+* `received-attachments`: new attachment metadata are enqueued here
+
+### Function Configuration
+The following must be set with `firebase functions:config:set`:
+
+* `mailgun.apikey`
+
+## Mailgun Configuration
+Setup a routing rule to store messages for the desired address.
+The `store` call should be configured with the URL to the `receiveEmail` function.
--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,32 @@
 var functions = require('firebase-functions');
+var pubSub = require('@google-cloud/pubsub')();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  console.log('request.body', request.body)
+const RECEIVED_ATTACHMENTS_TOPIC = 'received-attachments';
 
-  let body = '';
-  request.on('data', (data) => {
-    body += data;
+/* Attachments json:
+[
+  {
+    "url": "https://sw.api.mailgun.net/v3/domains/sandboxcb622ce2e73141bfb55e3970ce6cb7fa.mailgun.org/messages/eyJwIjpmYWxzZSwiayI6IjVlNzk4ODM4LTBhZjktNDg4NS1hZDRjLTNlYzZhNTcwOWZlMyIsInMiOiI5NmJlODgzMjgxIiwiYyI6InRhbmtiIn0=/attachments/0",
+    "content-type": "image/jpeg",
+    "name": "IMG_8138.jpg",
+    "size": 100243
+  }
+]
+*/
+exports.receiveEmail = functions.https.onRequest((request, response) => {
+  console.log('request.body', Object.keys(request.body));
+  console.log('attachments', request.body.attachments);
+  const attachments = request.body.attachments;
+  const pubSubMessage = {
+    attachments,
+  };
+  pubSub.topic(RECEIVED_ATTACHMENTS_TOPIC).publish(pubSubMessage, (err) => {
+    const status = err ? 500 : 200;
+    response.status(status).end();
   });
+});
 
-  request.on('end', () => {
-    console.log('raw body', body);
-    response.status(200).send();
-  });
-
+exports.receivedAttachmentsPubSub = functions.pubsub.topic(RECEIVED_ATTACHMENTS_TOPIC).onPublish(event => {
+  console.log('Got event data', event.data.json);
+  const eventData = event.data.json;
 });

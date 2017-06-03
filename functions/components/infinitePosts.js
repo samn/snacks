a309a2c3e621a6a501477e92748c864b467d6c99
Sam Neubardt <samneubardt@gmail.com>
2017-06-03 09:23:45 -0400

    Add inifinte scroll!

--- functions/components/infinitePosts.js
+++ functions/components/infinitePosts.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import React from 'react'
-import { AutoSizer, List, WindowScroller } from 'react-virtualized';
+import { AutoSizer, InfiniteLoader, List, WindowScroller } from 'react-virtualized';
 import PropTypes from 'prop-types';
+import fetch from 'isomorphic-fetch';
 
 export default class extends React.Component {
   static propTypes = {
@@ -12,29 +13,58 @@ export default class extends React.Component {
     super(props);
     this.state = {
       width: 0,
+      posts: props.posts,
     };
   }
 
   render() {
-    return <AutoSizer onResize={this.onResize} disableHeight>
-        {({ width }) => (
-          <WindowScroller>
-            {({ height, isScrolling, scrollTop }) => (
-              <List
-                autoHeight
-                ref={(c) => this.postList = c}
-                height={height}
-                isScrolling={isScrolling}
-                rowCount={this.props.posts.length}
-                rowHeight={this.rowHeight}
-                rowRenderer={this.renderPost}
-                scrollTop={scrollTop}
-                width={width}
-              />
-            )}
-          </WindowScroller>
-        )}
-      </AutoSizer>
+    // We don't know the total number of posts there are here, so use an arbitrarily large number (docs say this is ok).
+    return <InfiniteLoader
+      rowCount={1000}
+      isRowLoaded={this.isRowLoaded}
+      loadMoreRows={this.loadMoreRows}
+      threshold={5}>
+      {({ onRowsRendered, registerChild }) => (
+        <AutoSizer onResize={this.onResize} disableHeight>
+          {({ width }) => (
+            <WindowScroller>
+              {({ height, isScrolling, scrollTop }) => (
+                <List
+                  autoHeight
+                  ref={(c) => {
+                    registerChild(c);
+                    this.postList = c
+                  }}
+                  height={height}
+                  isScrolling={isScrolling}
+                  rowCount={this.state.posts.length}
+                  rowHeight={this.rowHeight}
+                  rowRenderer={this.renderPost}
+                  scrollTop={scrollTop}
+                  width={width}
+                  onRowsRendered={onRowsRendered}
+                />
+              )}
+            </WindowScroller>
+          )}
+        </AutoSizer>
+      )}
+    </InfiniteLoader>
+  }
+
+  isRowLoaded = ({index}) => {
+    return !!this.state.posts[index];
+  }
+
+  loadMoreRows = () => {
+    // fetch more posts "before" the last post in the list since they're in reverse chronological order
+    return fetch(`/fetchPosts?before=${_.last(this.state.posts).post_id}`)
+      .then(res => res.json())
+      .then(newPosts => {
+        this.setState((prevState, props) => ({
+          posts: prevState.posts.concat(newPosts),
+        }));
+      });
   }
 
   // Record the current width so image heights can be scaled
@@ -52,13 +82,13 @@ export default class extends React.Component {
   // so the list container can know the height of each image to calculate what's currently visible.
   // Scale the actual image height to the desired height based on the current width of the container.
   rowHeight = ({index}) => {
-    const post = this.props.posts[index];
+    const post = this.state.posts[index];
     const heightToWidth = post.image_height / post.image_width;
     return _.toInteger(this.state.width * heightToWidth);
   }
 
   renderPost = ({index, key, style}) => {
-    const post = this.props.posts[index];
+    const post = this.state.posts[index];
     // Set an explicit height (scaled proportionately off the window's width) so its dimensions will resize
     // with the appropriate ratio when the window resizes.
     return <img src={post.image_url} key={key} style={_.extend({}, style, { height: this.rowHeight({index}) })}/>;
--- functions/devServer.js
+++ functions/devServer.js
@@ -1,6 +1,8 @@
+const express = require('express');
 const next = require('next')
 const { createServer } = require('http')
 const cloudDatastore = require('@google-cloud/datastore')({
+  // this projectId should match what the datastore emulator is started with
   projectId: 'snacksdev',
 });
 const ObjectId = require('bson-objectid');
@@ -8,12 +10,16 @@ const ObjectId = require('bson-objectid');
 const PostsEntity = require('./lib/entities/posts');
 const postsEntity = new PostsEntity(cloudDatastore, "https://storage.googleapis.com/snacks-content");
 const makeRenderIndex = require('./lib/http/renderIndex');
+const makeFetchPosts = require('./lib/http/fetchPosts');
 
-const app = next({ dev: true });
-app.prepare().then(() => {
-  const renderIndex = makeRenderIndex(ObjectId, app.getRequestHandler(), postsEntity)
-  // TODO use express to route to renderIndex and API endpoints
-  createServer(renderIndex)
+const nextApp = next({ dev: true });
+nextApp.prepare().then(() => {
+  const renderIndex = makeRenderIndex(ObjectId, nextApp.getRequestHandler(), postsEntity)
+  const fetchPosts = makeFetchPosts(ObjectId, postsEntity);
+  const app = express();
+  app.get('/fetchPosts', fetchPosts);
+  app.get('/*', renderIndex);
+  createServer(app)
     .listen(3000, (err) => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')

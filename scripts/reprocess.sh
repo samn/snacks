2b021c6e6cbcdbe6420e06024f9de4f3e5676315
Sam Neubardt <samneubardt@gmail.com>
2017-06-15 20:58:25 -0400

    Add script to reprocess all uploads

---
+++ scripts/reprocess.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Given a GCS bucket, enqueue a message to reprocess each original upload image
+
+set -eu
+bucket=$1
+for full_path in `gsutil ls "gs://$bucket/originals/images"`; do
+  path=${full_path#gs://$bucket}
+  echo "Enqueuing reprocess for $path"
+  gcloud beta pubsub topics publish reprocess-images "{\"pathsToReprocess\": [\"$path\"]}"
+done
